75e8c8c529d541ac50ef01bc92b5b4c5602b162f
Lorenzo Scanu <Lorenzo Scanu>
2024-04-11 13:34:17 +0200

    compressioni finito

--- src/it/edu/iisgubbio/tpsit/CompressioneImmagine.java
+++ src/it/edu/iisgubbio/tpsit/CompressioneImmagine.java
@@ -1,4 +1,6 @@
 package it.edu.iisgubbio.tpsit;
+import java.util.Hashtable;
+
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
@@ -12,7 +14,16 @@ public class CompressioneImmagine extends Application{
 	int colonna = 3;
 	char lettere [][]= new char [colonna][riga];
 	TextField tPesoImmagine = new TextField();
+	Label lPesoImmagine = new Label("Peso immagine");
+	Label lPesoImmCompressa = new Label("Peso immagine compressa");
+	Label lCompressionRate = new Label("Compressio rate");
+	Hashtable<Integer,Integer> my_dict = new Hashtable<Integer,Integer>();
   public void start(Stage primaryStage) throws Exception {
+	  my_dict.put(1, 1);
+	  my_dict.put(2,2);
+	  my_dict.put(3,2);
+	  my_dict.put(4,3);
+	  
     GridPane griglia = new GridPane(); 
     griglia.setPadding(new Insets(5, 5, 5, 5));
     griglia.setHgap(10); 
@@ -20,9 +31,7 @@ public class CompressioneImmagine extends Application{
 	lettere[0][0]='V';lettere[0][1]='B';lettere[0][2]='B';lettere[1][0]='G';lettere[1][1]='G';lettere[1][2]='G';
 	lettere[2][0]='B';lettere[2][1]='V';lettere[2][2]='N';
 	Button bCalcola = new Button("verifica");
-	Label lPesoImmagine = new Label("Peso immagine");
-	Label lPesoImmCompressa = new Label("Peso immagine compressa");
-	Label lCompressionRate = new Label("Compressio rate");
+	
 	
 	griglia.add(lPesoImmagine, 0, 0);
 	griglia.add(tPesoImmagine, 1, 0);
@@ -39,38 +48,38 @@ public class CompressioneImmagine extends Application{
     bCalcola.setOnAction(e -> comprimi());
   }
   public void comprimi(){
-	  StringBuilder result = new StringBuilder();
       int rowCount = riga;
       int colCount = colonna;
-
-      for (int iColonna = 0; iColonna < colonna; iColonna++) {
+      char vAppoggio []= new char [9];
+      int iAppoggio =0;
+      for (int iRiga = 0; iRiga < colCount;iRiga++) {
           int count = 1;
-          for (int j = 1; j < colCount; j++) {
-              if (lettere[iColonna][j] == lettere[iColonna][j - 1]) {
-                  count++;
-              } else {
-                  result.append(lettere[iColonna][j - 1]).append(count);
-                  count = 1;
-              }
-          }
-          result.append(lettere[iColonna][colCount - 1]).append(count);
-          result.append("\n");
+          for (int iColonna = 0; iColonna < colonna; iColonna++) {
+        	  vAppoggio[iAppoggio]=lettere[iRiga][iColonna];
+        	  iAppoggio++;
+          } 
       }
+      int compressione=0;
+      int count =1;
+      for(int i=1; i<vAppoggio.length;i++) {
+    	  if(vAppoggio[i]== vAppoggio[i-1]) {
+    		  count++;
+    	  }else {
+    		  if(i==1) {
+    			  compressione=25;
+    		  }
+    		  compressione=compressione+(24+ my_dict.get(count));
+    		  System.out.println(compressione);
+    		  count=1;
+    	  }
+      }
+      lPesoImmCompressa.setText(lPesoImmCompressa.getText()+ " "+compressione);
+      double compressioneRate=100-(compressione*100.0/(24.0*(riga*colonna)));
+      System.out.println(vAppoggio);
+      lCompressionRate.setText(lCompressionRate.getText() +" "+compressioneRate);
   }
   
   public static void main(String args[]){
     launch();
   }
 }
-/*for (int iH=0; iH<riga;iH++ ) {
-for(int iL =0 ; iL< colonna;iL++ ) {;
-	char lettera;
-	lettera = ((char)(Math.random()*('Z'-'A'+1)+'A'));
-	Label lCasella = new Label (""+lettera);
-	labelLettere[iH][iL]=lCasella;
-	lettere[iH][iL]=lettera;
-	//bCasella.setId("ButtonTrue");
-	lCasella.setPrefSize(50, 30);
-	griglia.add(lCasella, iH,iL );
-}
-}*/
